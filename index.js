582c5c606fb227d444f8ed836b49759127de2a8c
pixShopping <pixshopping021@gmail.com>
2023-11-30 12:56:36 -0300

    testando coisas 2

--- index.js
+++ index.js
@@ -16,78 +16,51 @@ async function autoFillAndSubmitForm(usuario, senha, mensagem) {
     });
 
     const page = await browser.newPage();
-
+    
     await page.goto('https://cmspweb.ip.tv/'); // URL do site
   
     const ra = usuario.slice(0,12);
     const digito = usuario.slice(-1);
     
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-
-    const acesso = await page.waitForSelector('#access-student');
-    if (acesso) {
-      await page.click('#access-student');
-    } else {
-      console.log('botao acesso não encontrado')
-    }
+    await page.waitForSelector('#access-student');
+    await page.click('#access-student');
     
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     await page.waitForSelector('#ra-student');
     await page.type('#ra-student', ra);
 
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-
     await page.waitForSelector('#digit-student');
     await page.type('#digit-student', digito);
-
-    await new Promise((resolve) => setTimeout(resolve, 5000));
     
     await page.waitForSelector('#password-student');
     await page.type('#password-student', senha);
 
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-
     // Clique no botão de login (substitua o seletor apropriado)
-    const logar = await page.waitForSelector('#btn-login-student');
-    if (logar) {
-      await page.click('#btn-login-student');
-    } else {
-      console.log('botao logar não encontrado')
-    }
-
+    await page.waitForSelector('#btn-login-student');
+    await page.click('#btn-login-student');
+    
     await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
     // Espere segundos antes de continuar a execução
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     
-    await page.screenshot({ path: 'screenshot.png' });
+    await page.waitForSelector('#lproom_img_reccf9a77bbad38831-l');
+    await page.click('#lproom_img_reccf9a77bbad38831-l');
 
-    /*const serie = await page.waitForSelector('#lproom_reccf9a77bbad38831-l');
-    if (serie) {
-      await page.click('#lproom_reccf9a77bbad38831-l');
-    } else {
-      console.log('botao serie não encontrado')
-    }
-
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-
-    const sala = await page.waitForSelector('#roomDetailConference');
-    if (sala) {
-      await page.click('#roomDetailConference');
-    } else {
-      console.log('botao sala não encontrado')
-    }
-
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    
+    await page.waitForSelector('#roomDetailConference');
+    await page.click('#roomDetailConference');
+    
     await new Promise((resolve) => setTimeout(resolve, 15000));
 
-    // Vitor Custódio da Silva, N° 36 está presente
     // Clica na sala de aula virtual
     await page.waitForSelector('#rpchntx');
     await page.type('#rpchntx', mensagem);
-    await page.keyboard.press('Enter');*/
+    await page.keyboard.press('Enter');
 
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     
     await browser.close();
 
